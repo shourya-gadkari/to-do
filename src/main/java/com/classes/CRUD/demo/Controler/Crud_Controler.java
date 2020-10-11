fcfd9960ff7ae93221e976b724b1ee16173e18d8
unknown <gadkarishourya@gmail.com>
2020-10-11 10:19:51 +0530

    SpringBootWebsite

--- src/main/java/com/classes/CRUD/demo/Controler/Crud_Controler.java
+++ src/main/java/com/classes/CRUD/demo/Controler/Crud_Controler.java
@@ -3,16 +3,17 @@ package com.classes.CRUD.demo.Controler;
 
 import com.classes.CRUD.demo.Service.Main;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+
+@Controller
 public class Crud_Controler {
     @Autowired
     Main main;
-    @RequestMapping("/home")
+    @RequestMapping("/")
     public String home(){
         return "index";
     }
